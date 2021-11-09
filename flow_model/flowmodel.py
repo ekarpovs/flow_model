22b8208420683377028f691fdd0688af2ca96e02
Evgeny Karpovsky <ekarpovsky@gmail.com>
2021-11-09 15:03:27 +0200

    Remove begin/end markers

--- flow_model/flowmodel.py
+++ flow_model/flowmodel.py
@@ -3,9 +3,6 @@ from typing import List, Dict, Tuple
 from .flowitemtype import FlowItemType
 from .flowitemmodel import FlowItemModel
 
-BEGIN_FLOW_MARKER = {"stm": "glbstm.begin"}
-END_FLOW_MARKER = {"stm": "glbstm.end"}
-
 class FlowModel():
   '''
   Flow model
@@ -49,8 +46,6 @@ class FlowModel():
   def _parse(worksheet: List[Dict]) -> Tuple[str, List[FlowItemModel]]:
     info = ''
     items: List[FlowItemModel] = []
-    item = FlowItemModel(FlowItemType.STM_BEGIN, 'glbstm.begin')
-    items.append(item)
     for step in worksheet:
       if 'info' in step:
         info = step.get('info')
@@ -59,18 +54,12 @@ class FlowModel():
       iname = ''
       params = {}
       aliases = {}
-      if 'exec' in step:
-        type = FlowItemType.EXEC
-        iname = step.get('exec')
-      elif 'stm' in step:
-        type = FlowItemType.STM_BEGIN
-        iname = step.get('stm')
+      type = FlowItemType.EXEC
+      iname = step.get('exec')
       if 'params' in step:
         params = step.get('params')
       if 'aliases' in step:
         aliases = step.get('aliases')
       item = FlowItemModel(type, iname, params, aliases)
       items.append(item)
-    item = FlowItemModel(FlowItemType.STM_END, 'glbstm.end')
-    items.append(item)
     return (info, items)
